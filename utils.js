9136a4afc5de4513e5ab6120c6c5e0327afb1738
Damiano <zappia.1870958@studenti.uniroma1.it>
2020-07-25 19:43:04 +0200

    comments update

--- utils.js
+++ utils.js
@@ -1,4 +1,4 @@
-//interpolation function
+//interpolation function, used for the rotation of the eagle
 function lerp(current, target, fraction){
 
     var array_of_points = [];
@@ -10,45 +10,40 @@ function lerp(current, target, fraction){
   return (array_of_points);
   }
 
-
+//interpolation function, used to make the veihcles turn
   function lerp2(current, target, fraction){
   	return (current + fraction *(target-current));
   }
 
-
+// quadric bezier curve function
   function bezier1(p0, p1, p2, fraction){
 
     var q_0 = [];
 
     for (var is = 0; is < (1/fraction); is++){
       var j = is*fraction;
-      //l_0.push(p0*(1-j)+p1*j); //L0(t)
-      //l_1.push(p1*(1-j)+p2*j); //L1(t)
+
       q_0.push( (((1-j)**2)*p0) + (2*(1-j)*j*p1) + ((j**2) *p2))
 
-      //c_0.push( p0*(1-j)**3 + 3*((1-j)**2)*j*p1 + 3*(1-j)*(j**2)*p2 + (j**3)*p3 )
     }
     return (q_0);
 
   }
 
+// cubic bezier curve function
   function bezier2(p0, p1, p2, p3, fraction){
 
     var c_0 = [];
 
     for (var is = 0; is < (1/fraction); is++){
       var j = is*fraction;
-      //l_0.push(p0*(1-j)+p1*j); //L0(t)
-      //l_1.push(p1*(1-j)+p2*j); //L1(t)
-      //q_0.push( (((1-j)**2)*p0) + (2*(1-j)*j*p1) + ((j**2) *p2))
-
       c_0.push( p0*(1-j)**3 + 3*((1-j)**2)*j*p1 + 3*(1-j)*(j**2)*p2 + (j**3)*p3 )
     }
     return (c_0);
 
   }
 
-
+// function to plot the scenegraph of an object
   function dumpObject(obj, lines = [], isLast = true, prefix = '') {
     const localPrefix = isLast ? '└─' : '├─';
     lines.push(`${prefix}${prefix ? localPrefix : ''}${obj.name || '*no-name*'} [${obj.type}]`);
